d85243f9a71c61cea7d56611e93961eaf14630e9
Louis Sanyenni <lsanyenni@gmail.com>
2026-01-12 15:36:11 +0000

    Print statements in Java

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
-
+//This is my first java program
    public static void main(String[] args){
-     System.out.print("I love pizza!");
+     System.out.println("I love pizza!");
+     System.out.println("It's really good!");
+     System.out.println("Buy me pizza!");
+     
+
+
     }
 }
