98564b4ab58d16aa3b0cead97a82481afb1364ec
Louis Sanyenni <lsanyenni@gmail.com>
2026-01-12 16:09:20 +0000

    variables in Java

--- Main.java
+++ Main.java
@@ -5,6 +5,32 @@ public class Main {
      System.out.println("It's really good!");
      System.out.println("Buy me pizza!");
      
+//Variable in Java
+// primitive = simple value stored directly in memory (stack)
+// Reference = memory address (stack) that point to the (heap)
+
+int age = 21;
+int year =2025;
+
+double price = 19.99;
+double gpa = 3.5;
+double temperature =-12.5;
+
+char grade = 'A';
+char symbol = '!';
+char currency = '$';
+
+boolean isStudent = true;
+boolean forSale = false;
+
+
+System.out.println(age);
+System.out.println("The year is "+ year);
+System.out.println("$" + price);
+System.out.println (currency +" " + price);
+
+System.out.println(forSale);
+
 
 
     }
