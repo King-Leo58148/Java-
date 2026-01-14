231a32e87bdf1d8ea31fdc278b8833c9f9a21ea9
Louis Sanyenni <lsanyenni@gmail.com>
2026-01-14 20:41:46 +0000

    bit lazy today

--- work.java
+++ work.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class work {
  public static void main(String[] args){
     double[] numbers = {1,2,3,4,5,6,7,8,9,10};
@@ -6,8 +6,15 @@ public class work {
     for(int i=0;i<10;i++){
         sum+=numbers[i];
     }
+
     double average = sum/10;
     
+    double deviation_sum = 0;
+    for (int i = 0; i < 10; i++) {
+        deviation_sum += Math.pow(numbers[i] - average,2);
+       
+    }
+    double standard_deviation=Math.sqrt(deviation_sum/9);
 
 
     System.out.println("Average : " + average);
