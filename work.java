4627effe30bb0f0d08e2145dfbf62b127e9e450b
Louis Sanyenni <lsanyenni@gmail.com>
2026-01-16 15:55:19 +0000

    java assignment

--- work.java
+++ work.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class work {
  public static void main(String[] args){
     double[] numbers = {1,2,3,4,5,6,7,8,9,10};
@@ -15,16 +14,20 @@ public class work {
        
     }
     double standard_deviation=Math.sqrt(deviation_sum/9);
-    
     int num = (int) Math.round(standard_deviation);
-    int factorial;
-    for(int i = 0 ; i<num ;i++){
-     factorial= (num)
-    }
+    int factorial=1;
 
+    for (int i = 0; i<10;i++){
+        int n = (10-i);
+        factorial= factorial * n;
+
+
+    
+    }
 
     System.out.println("Average : " + average);
     System.out.println("Standard deviation : " + standard_deviation);
+    System.out.println(factorial);
 
 
  }
