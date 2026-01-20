d89c324b45a230b15dd58b150e311b4ebc731742
King-Leo58148 <lsanyenni@gmail.com>
2026-01-20 21:23:26 +0000

    mad lab game

--- brocode.java
+++ brocode.java
@@ -1,11 +1,31 @@
 import java.util.Scanner;
-public class Jude{
-    public static void main (String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name=sc.nextLine();
-        System.out.println("Hello "+name);
-    }
-}
 
-//debuged a Java program that takes user input and prints a greeting message.
+public class brocode {
+    //mad labs
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String adjective2;
+        String noun;
+        String verb;
+        String adjective3;
+        System.out.println("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+        System.out.println("Enter a noun (animal or person): ");
+        noun = scanner.nextLine();
+        System.out.println("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+        System.out.println("Enter a verb ending with -ing (action): ");
+        verb = scanner.nextLine();
+        System.out.println("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
+
+        System.out.println("Today I went to a " +adjective1 + " zoo.");
+         System.out.println("In an exhibit I saw a "+ noun + ".");
+         System.out.println(noun + " was " + adjective2 + " and "+ verb + "!");
+        System.out.println(" I was "+ adjective3 + "!");
+        
+    
+    }
+    }
