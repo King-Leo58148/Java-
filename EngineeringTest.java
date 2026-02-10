314ef36e3b6a389290b8b1b197ec06171dfc546f
King-Leo58148 <lsanyenni@gmail.com>
2026-02-10 16:10:18 +0000

    EngineeringTest

---
+++ EngineeringTest.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class EngineeringTest {
+ 
+    public static void main(String[] args) {
+        double structralIntegrity = 100.0;
+        double failureThreshold =15;
+        int cycleCount = 0;
+        double fatigueDamage = 5.0;
+        
+        Scanner sc = new Scanner(System.in);
+        while(structralIntegrity >failureThreshold){
+           System.out.print("Enter pressure force for Cycle "+ (cycleCount+1) + ":");
+           double force = sc.nextDouble();
+           if (force >20.0){
+            structralIntegrity-=(force+fatigueDamage);
+
+           }
+           else{
+           structralIntegrity-=force;
+
+           }
+           System.out.println("Remaining Integrity: " + structralIntegrity);
+          
+          cycleCount++;
+        }
+
+        System.out.println("Material Failure at Cycle "+ cycleCount);
+        System.out.println("Final Structural Integrity: " + structralIntegrity);
+        sc.close();
+
+    }
+
+
+}
