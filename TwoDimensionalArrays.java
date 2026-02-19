678b37722df10c507a6f238894eda7b5c6e0a228
King-Leo58148 <lsanyenni@gmail.com>
2026-02-19 15:39:47 +0000

    Two dimensional array

---
+++ TwoDimensionalArrays.java
@@ -0,0 +1,11 @@
+public class TwoDimensionalArrays {
+    public static void main(String [] args){
+        int [][] n = {
+            {7,4,8},
+            {2,5,6},
+            {9,8,4},
+            {4,2,1}};
+
+       }
+    
+}
