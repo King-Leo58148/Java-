4fb33c128f175993e44739c0f6a022188a4906f6
King-Leo58148 <lsanyenni@gmail.com>
2026-04-14 21:14:44 +0000

    overloaded constructors

--- Main.java
+++ Main.java
@@ -15,18 +15,31 @@ public class Main {
     //   car1.drive();
     //   car1.brake();
     
-    Student student1 = new Student("Spongebob",30,3.2);
-    Student student2 = new Student("Patrick",35,2.8);
-    System.out.println(student1.name);
-    System.out.println( student1.age);
-    System.out.println( student1.gpa);
-     System.out.println( student1.isEnrolled);
-     System.out.println(student2.name);
-    System.out.println( student2.age);
-    System.out.println( student2.isEnrolled);
+    // Student student1 = new Student("Spongebob",30,3.2);
+    // Student student2 = new Student("Patrick",35,2.8);
+    // System.out.println(student1.name);
+    // System.out.println( student1.age);
+    // System.out.println( student1.gpa);
+    //  System.out.println( student1.isEnrolled);
+    //  System.out.println(student2.name);
+    // System.out.println( student2.age);
+    // System.out.println( student2.isEnrolled);
  
+    User user1 = new User("Louis");
+    User user2 = new User("Sandy","sandy@gmail.com");
+    User user3 = new User("Patrick","patrickstar@gmail.com",21);
     
-    
+    System.out.println(user1.name);
+     System.out.println(user1.email);
+      System.out.println(user1.age);
+
+      System.out.println(user2.name);
+      System.out.println(user2.email);
+      System.out.println(user2.age);
+
+      System.out.println(user3.name);
+      System.out.println(user3.email);
+      System.out.println(user3.age);
     
 }}
 
