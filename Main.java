ae5eea25cb9b9cb0960c30674f66bc4d5934c565
King-Leo58148 <lsanyenni@gmail.com>
2026-04-14 20:47:56 +0000

    commit

--- Main.java
+++ Main.java
@@ -4,14 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
         
-      Car car = new Car();
-     car.start();
-      System.out.println(car.model);
-      System.out.println(car.year);
-      System.out.println(car.make);
-      System.out.println(car.isRunning);
-      car.drive();
-      car.brake();
+    //   Car car1 = new Car();
+    //   Car car2 = new Car();
+    //   car1.start();
+    //   System.out.println(car1.model);
+    //   System.out.println(car2.model);
+    //   System.out.println(car1.year);
+    //   System.out.println(car1.make);
+    //   System.out.println(car1.isRunning);
+    //   car1.drive();
+    //   car1.brake();
+    
+    Student student1 = new Student("Spongebob",30,3.2);
+    Student student2 = new Student("Patrick",35,2.8);
+    System.out.println(student1.name);
+    System.out.println( student1.age);
+    System.out.println( student1.gpa);
+     System.out.println( student1.isEnrolled);
+     System.out.println(student2.name);
+    System.out.println( student2.age);
+    System.out.println( student2.isEnrolled);
+ 
+    
+    
     
 }}
 
