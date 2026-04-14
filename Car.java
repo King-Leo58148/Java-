658e2fe8b41157fcd4486ed5da483932e4efdee9
King-Leo58148 <lsanyenni@gmail.com>
2026-04-14 20:22:15 +0000

    oop

---
+++ Car.java
@@ -0,0 +1,29 @@
+public class Car{
+    String make = "Ford";
+    String model ="Mustard";
+    int year = 2025;
+    double price = 500000.99;
+    boolean isRunning = false;
+    
+
+    void start(){  
+        isRunning = true;
+        System.out.println("You started the engine!");
+    }
+
+    void stop(){
+        isRunning = false;
+        System.out.println("You stopped the engine!");
+    }
+
+    void drive(){
+        System.out.println("You drive the " + model);
+
+}
+
+    void brake(){
+         System.out.println("You brake the "+ model);
+}
+
+
+}
--- Main.java
+++ Main.java
@@ -1,74 +1,17 @@
-import java.util.Scanner;
 
 public class Main {
-//This is my first java program
-   public static void main(String[] args){
-     System.out.println("I love pizza!");
-     System.out.println("It's really good!");
-     System.out.println("Buy me pizza!");
-     
-//Variable in Java
-// primitive = simple value stored directly in memory (stack)
-// Reference = memory address (stack) that point to the (heap)
-
-int age = 21;
-int year =2025;
-
-double price = 19.99;
-double gpa = 3.5;
-double temperature =-12.5;
-
-char grade = 'A';
-char symbol = '!';
-char currency = '$';
-
-boolean isStudent = true;
-boolean forSale = false;
 
 
-String name = "Louis";
-String email = "lsanyenni@gmail.com";
+    public static void main(String[] args) {
+        
+      Car car = new Car();
+     car.start();
+      System.out.println(car.model);
+      System.out.println(car.year);
+      System.out.println(car.make);
+      System.out.println(car.isRunning);
+      car.drive();
+      car.brake();
+    
+}}
 
-System.out.println(age);
-System.out.println("The year is "+ year);
-System.out.println("$" +" price");
-System.out.println (currency + " " + price);
-
-System.out.println(forSale);
-
-
-//simple if statement
-if(isStudent){
-    System.out.println("You are a student");
-}
-
-else {
-    System.out.println("You are not a student");
-}
-
-
-
-System.out.println("My name is "+ name);
-System.out.println("Your email is "+ email);
-
-
-Scanner scanner  = new Scanner(System.in);  
-System.out.print("Enter your name: ");
-String name1 = scanner.nextLine();
-System.out.println("Hello "+ name1);
-System.out.print("Enter you age: ");
-int age1 = scanner.nextInt();
-System.out.println("You are "+ age1 + " years old");
-
-System.out.print("What is your GPA?: ");
-double gpa1 = scanner.nextDouble();
-System.out.println("Your gpa is " + gpa1);
-
-String name3 = scanner.nextLine();
-scanner.close();
-
-int x = 5*3/2 -3%5 +2;
-System.out.println(x);
-
-}
-}
